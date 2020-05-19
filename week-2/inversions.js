7446c3d9e61aedf44681acb154b8fc0c3bea19ef
xiaofeizhang19 <xzhang613@gmail.com>
2020-05-19 22:27:44 +0100

    Add functions to count inversions in array

--- week-2/inversions.js
+++ week-2/inversions.js
@@ -1,14 +1,42 @@
 const fs = require('fs')
 const ints = fs.readFileSync('integers.txt', 'utf-8').split('\n');
 
-const sortAndCount = (arr1, arr2) => {
+const merge = (left, right) => {
   let sorted = [];
-  while (arr1.length && arr2.length) {
-    
+  let count = 0;
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+      count += left.length;
+    }
   }
+  return [sorted.concat(left.slice().concat(right.slice())), count];
 }
-const inversions = arr => {
+const inversions = (arr, lo, hi) => {
+  if (lo === hi) {
+    return [[arr[lo]], 0];
+  }
 
+  // const mid = Math.floor(arr.length / 2);
+  // const left = arr.slice(0, mid);
+  // const right = arr.slice(mid);
+
+  const mid = Math.floor(lo + (hi - lo) / 2)
+
+  let [leftMerged, leftCount] = inversions(arr, lo, mid);
+  let [rightMerged, rightCount] = inversions(arr, mid + 1, hi);
+  let [merged, mergeCount] = merge(leftMerged, rightMerged);
+
+  return [merged, leftCount + rightCount + mergeCount];
 }
 
-module.exports = inversions;
+const get_inversions = arr => {
+  return inversions(arr, 0, arr.length - 1)[1]
+}
+
+console.log(get_inversions(ints));
+
+// module.exports = inversions;
